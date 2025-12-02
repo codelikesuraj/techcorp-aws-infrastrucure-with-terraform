95e2fb6dc01868aba1ce9bc8998555b300e47dac
Abdulbaki Suraj <codelikesuraj@gmail.com>
2025-12-02 11:45:43 +0100

    initial setup

---
+++ main.tf
@@ -0,0 +1,153 @@
+terraform {
+  required_version = ">=1.4.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+#####################################
+# AWS provider
+#####################################
+
+provider "aws" {
+  region = var.region
+}
+
+#####################################
+# VPC
+#####################################
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "techcorp-vpc"
+  }
+}
+
+#####################################
+# Availability Zones
+#####################################
+
+data "aws_availability_zones" "available" {
+    region = var.region
+    state = "available"
+}
+
+#####################################
+# Subnets 
+#####################################
+
+# Public subnet 1
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_1_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "techcorp-public-subnet-1"
+  }
+}
+
+# Public subnet 2
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_2_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  
+  tags = {
+    Name = "techcorp-public-subnet-2"
+  }
+}
+
+# Private subnet 1
+resource "aws_subnet" "private_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_1_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "techcorp-private-subnet-1"
+  }
+}
+
+# Private subnet 2
+resource "aws_subnet" "private_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_2_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "techcorp-private-subnet-2"
+  }
+}
+
+#####################################
+# Internet Gateway
+#####################################
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "techcorp-igw"
+    }
+}
+
+#####################################
+# Elastic IPs for NAT Gateways
+#####################################
+
+# Elastic IP 1
+resource "aws_eip" "eip_1" {
+    domain = "vpc"
+
+    tags =  {
+        Name = "techcorp-nat-eip-1"
+    }
+
+    depends_on = [aws_internet_gateway.main]
+}
+
+# Elastic IP 2
+resource "aws_eip" "eip_2" {
+    domain = "vpc"
+
+    tags = {
+        Name = "techcorp-nat-eip-2"
+    }
+    depends_on = [aws_internet_gateway.main]
+}
+
+#####################################
+# NAT Gateways
+#####################################
+
+# NAT Gateway 1
+resource "aws_nat_gateway" "nat_1" {
+    allocation_id = aws_eip.eip_1.id
+    subnet_id = aws_subnet.public_1.id
+
+    tags = {
+        Name = "techcorp-nat-1"
+    }
+
+    depends_on = [aws_internet_gateway.main]
+}
+
+# NAT Gateway 2
+resource "aws_nat_gateway" "nat_2" {
+    allocation_id = aws_eip.eip_2.id
+    subnet_id = aws_subnet.public_2.id
+
+    tags = {
+        Name = "techcorp-nat-2"
+    }
+
+    depends_on = [aws_internet_gateway.main]
+}
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for private subnet 1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for private subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
