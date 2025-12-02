3fe0cf167136592d43ef00e84ec0170164b2bb1f
Abdulbaki Suraj <codelikesuraj@gmail.com>
2025-12-02 13:10:12 +0100

    added route tables and associations for public & private subnets

--- main.tf
+++ main.tf
@@ -151,3 +151,68 @@ resource "aws_nat_gateway" "nat_2" {
 
     depends_on = [aws_internet_gateway.main]
 }
+
+#####################################
+# Route Tables
+#####################################
+
+# Public Route Table with Association
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "techcorp-rt-public"
+    }
+}
+
+resource "aws_route_table_association" "public_1" {
+    subnet_id = aws_subnet.public_1.id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+    subnet_id = aws_subnet.public_2.id
+    route_table_id = aws_route_table.public.id
+}
+
+# Private Route Tables (one per AZ) with Association
+resource "aws_route_table" "private_1" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_1.id
+    }
+
+    tags = {
+        Name = "techcorp-rt-private-1"
+    }
+}
+
+resource "aws_route_table_association" "private_1" {
+    subnet_id = aws_subnet.private_1.id
+    route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_route_table" "private_2" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_2.id
+    }
+
+    tags = {
+        Name = "techcorp-rt-private-2"
+    }
+}
+
+resource "aws_route_table_association" "private_2" {
+    subnet_id = aws_subnet.private_2.id
+    route_table_id = aws_route_table.private_2.id
+}
